a041e3747c352f6f37e96c55f51df07c2bb52040
Pedro Henrique <126733190+hsspedro@users.noreply.github.com>
2023-09-13 19:48:54 -0300

    Update 1534.c

--- Question/1534.c
+++ Question/1534.c
@@ -8,15 +8,15 @@ int main() {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-				if (i == j){
-					matriz[i][j] = 1;
-				}
-				if (i == N - j - 1){
-					matriz[i][j] = 2;
-				}
-				if (i != j && i != N - j - 1){
-					matriz[i][j] = 3;
-				}
+		if (i == j){
+			matriz[i][j] = 1;
+		}
+		if (i == N - j - 1){
+			matriz[i][j] = 2;
+		}
+		if (i != j && i != N - j - 1){
+			matriz[i][j] = 3;
+		}
             }
         }
 
